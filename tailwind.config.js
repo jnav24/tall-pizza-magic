44af6049d04e3b9e8a092b4e9bf656b9e89658eb
Justin Navarro <info@justinnavarro.net>
2024-05-20 04:22:03 -0400

    update the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,25 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
     theme: {
         extend: {
+            colors: {
+                danger: colors.red["700"],
+                'danger-hover': colors.red["600"],
+                'danger-focus': colors.red["800"],
+                primary: colors.orange["400"],
+                'primary-hover': colors.orange["300"],
+                'primary-focus': colors.orange["500"],
+                success: colors.green["600"],
+            },
             fontFamily: {
                 sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            width: {
+                100: '25rem',
+                150: '38rem',
+                200: '50rem',
             },
         },
     },
