af79e18d585a4a07836c4e0652a88c55d45918ba
Justin Navarro <info@justinnavarro.net>
2024-05-20 04:29:38 -0400

    order status and factory

---
+++ database/factories/OrderStatusFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\OrderStatus;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class OrderStatusFactory extends Factory
+{
+    protected $model = OrderStatus::class;
+
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name(),
+        ];
+    }
+}
